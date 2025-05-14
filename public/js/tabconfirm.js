97849a1da38379b316e48cabeb8cb14cc667d3c7
467riguy8 <ezownerofinfinitescript@gmail.com>
2025-05-14 15:11:23 -0500

    Delete public directory

--- public/js/tabconfirm.js
+++
@@ -1,8 +0,0 @@
-function confirmtab() {
-    const selectElement = document.querySelector('#tabselect');
-    const selectedValue = selectElement.value;
-    localStorage.setItem('sitefortab', selectedValue);
-    localStorage.setItem('tabselectIndex', selectElement.selectedIndex);
-    alert("panic site set to " + localStorage.getItem('sitefortab'));
-    location.reload()
-}
